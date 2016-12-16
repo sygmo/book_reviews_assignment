93ed7b068d18572d6e21439b49842aa9c306a68e
sygmo <sydney.mercier91@gmail.com>
2016-12-15 21:41:41 -0600

    urls fixes to get first page to display

--- apps/bookreviews/urls.py
+++ apps/bookreviews/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^books$', views.index, name='bookreviews-index'),
-	url(r'^add$', views.index, name='add-book'),
-	url(r'^books/(?P<id>\d+)$', views.index, name='book-info'),
+	url(r'^$', views.index, name='bookreviews-index'),
+	#url(r'^add$', views.index, name='add-book'),
+	#url(r'^books/(?P<id>\d+)$', views.index, name='book-info'),
 ]
